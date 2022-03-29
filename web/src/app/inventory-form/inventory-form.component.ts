6b18c159b3de6f2570786d6e65043a9c8aba1516
Feny Mehta <fbm3307@gmail.com>
2022-03-29 16:29:02 +0530

    UI Testing

--- web/src/app/inventory-form/inventory-form.component.ts
+++ web/src/app/inventory-form/inventory-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AddInventory } from '../inventory';
+import { InventoryService } from '../inventory.service';
 @Component({
   selector: 'app-inventory-form',
   templateUrl: './inventory-form.component.html',
@@ -7,8 +8,10 @@ import { AddInventory } from '../inventory';
 })
 export class InventoryFormComponent implements OnInit {
   model= new AddInventory(0,"","",false,0)
-  constructor() { }
+  constructor(private inventoryService: InventoryService) { }
   ngOnInit(): void {
   }
-
+  newInventory(){
+    this.inventoryService.createInventory(this.model.name,this.model.description, this.model.price)
+  }
 }
--- web/src/app/inventory.service.ts
+++ web/src/app/inventory.service.ts
@@ -16,4 +16,10 @@ export class InventoryService {
   getInventory(): Observable<Inventory[]> {
     return this.http.get<Inventory[]>(this.inventoryUrl);
   }
+
+  createInventory(name: String, description: String, price: Number): Observable<Inventory> {
+    var inventory= {name:name, description:description, price:price, status:true}
+    return this.http.post<Inventory>(this.inventoryUrl, inventory, this.httpOptions);
+  }
+
 }
